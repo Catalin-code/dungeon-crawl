4b7e0e4adb738b81b8cd059fe1a7df5c367d0806
Andrei Stancu <candre020198@gmail.com>
2020-10-15 09:40:26 +0300

    differentMonsters implemented

--- src/main/java/com/codecool/dungeoncrawl/Main.java
+++ src/main/java/com/codecool/dungeoncrawl/Main.java
@@ -58,22 +58,18 @@ public class Main extends Application {
         switch (keyEvent.getCode()) {
             case UP:
                 map.getPlayer().move(0, -1);
-//                map.getGhost().move(x, y-1);
                 refresh();
                 break;
             case DOWN:
                 map.getPlayer().move(0, 1);
-//                map.getGhost().move(x, y + 1);
                 refresh();
                 break;
             case LEFT:
                 map.getPlayer().move(-1, 0);
-//                map.getGhost().move(-1, 0);
                 refresh();
                 break;
             case RIGHT:
                 map.getPlayer().move(1, 0);
-//                map.getGhost().move(1,0);
                 refresh();
                 break;
         }
